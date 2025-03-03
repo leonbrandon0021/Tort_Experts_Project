334effa53f9da3cd763c93855a5752d0bd7eb8f8
leonbrandon0021 <leonbrandon21@gmail.com>
2025-03-03 13:44:07 -0600

    Create query1_updating_field_names.sql

---
+++ queries/query1_updating_field_names.sql
@@ -0,0 +1,37 @@
+﻿ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN string_field_0 to InvoiceId;
+
+/*
+ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN string_field_1 to StockCode;
+*/
+
+/*
+ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN string_field_2 to Description;
+*/
+
+/*
+ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN int64_field_3 to Quantity;
+*/
+
+/*
+ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN timestamp_field_4 to InvoiceDate;
+*/
+
+/*
+ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN double_field_5 to UnitPrice;
+*/
+
+/*
+ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN int64_field_6 to CustomerID;
+*/
+
+/*
+ALTER TABLE `retailmarketingproject.data.retail_marketing_data` 
+RENAME COLUMN string_field_7 to Country;
+*/
