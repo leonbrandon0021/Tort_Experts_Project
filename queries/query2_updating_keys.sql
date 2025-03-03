01dabb466c09e3aec879dffedcb9afd4694ccdee
leonbrandon0021 <leonbrandon21@gmail.com>
2025-03-03 16:17:56 -0600

    Update query2_updating_keys.sql

--- queries/query2_updating_keys.sql
+++ queries/query2_updating_keys.sql
@@ -1,7 +1,14 @@
-﻿ALTER TABLE retailmarketingproject.data.retail_marketing_data 
+﻿--GBQ does not allow for enforcement of uniqueness after a table was created. Therefore NOT ENFORCED is required
+
+ALTER TABLE retailmarketingproject.data.retail_marketing_data 
 ADD PRIMARY KEY (invoiceid) NOT ENFORCED --GBQ must have NOT ENFORCED constraints;
 
+
+
 /*
+
+--No customers table exists in my GBQ environment but this statement would create a foreign key to create a relationship between the tables
+
 ALTER TABLE retailmarketingproject.data.retail_marketing_data 
 ADD FOREIGN KEY (customerid) REFERENCES retailmarketingproject.data.cutomers (customerid) NOT ENFORCED
 */
