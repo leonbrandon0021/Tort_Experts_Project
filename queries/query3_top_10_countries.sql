6c3a5c12d2c95ed147f4eb87e2332dcb39d6e5d4
leonbrandon0021 <leonbrandon21@gmail.com>
2025-03-03 15:01:59 -0600

    Create query3_top_10_countries.sql

---
+++ queries/query3_top_10_countries.sql
@@ -0,0 +1,22 @@
+﻿with countries as(
+  select
+  Country
+  ,count(distinct invoiceid) as number_of_transactions
+  ,sum(quantity) as total_number_of_products_sold
+  ,round(sum(UnitPrice),2) as total_revenue
+  from retailmarketingproject.data.retail_marketing_data
+  group by
+  Country
+  order by 4 desc
+)
+
+select
+*
+from(
+  select 
+  dense_rank() over(order by total_revenue desc) as rank
+  ,* 
+  from countries
+)
+where rank <= 10
+order by rank
