8223db4a779ebbdaf4e2e8e3328037ffb83e3812
leonbrandon0021 <leonbrandon21@gmail.com>
2025-03-03 15:01:53 -0600

    Delete query2_updating_contraints.sql

--- queries/query2_updating_contraints.sql
+++
@@ -1,7 +0,0 @@
-﻿ALTER TABLE retailmarketingproject.data.retail_marketing_data 
-ADD PRIMARY KEY (invoiceid) NOT ENFORCED --GBQ must have NOT ENFORCED constraints;
-
-/*
-ALTER TABLE retailmarketingproject.data.retail_marketing_data 
-ADD FOREIGN KEY (customerid) REFERENCES retailmarketingproject.data.cutomers (customerid) NOT ENFORCED
-*/
