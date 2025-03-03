ecdceb8a34d49a697b700d16f20ca7408624e4dd
leonbrandon0021 <leonbrandon21@gmail.com>
2025-03-03 16:08:31 -0600

    Create query5_duplicates.sql

---
+++ queries/query5_duplicates.sql
@@ -0,0 +1,21 @@
+﻿WITH duplicates as(
+  SELECT
+  invoiceid,StockCode,Description,Quantity,InvoiceDate,UnitPrice,CustomerID,Country
+  FROM retailmarketingproject.data.retail_marketing_data
+  GROUP BY
+  invoiceid,StockCode,Description,Quantity,InvoiceDate,UnitPrice,CustomerID,Country
+  HAVING COUNT(*) > 1
+)
+
+SELECT
+d.*
+FROM duplicates d
+inner join retailmarketingproject.data.retail_marketing_data t
+on d.invoiceid = t.invoiceid
+and d.StockCode = t.StockCode
+and d.Description = t.Description
+and d.Quantity = t.Quantity
+and d.InvoiceDate = t.InvoiceDate
+and d.UnitPrice = t.UnitPrice
+and d.CustomerID = t.CustomerID
+and d.Country = t.Country
